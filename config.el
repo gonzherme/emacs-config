4cb709dca5da5b0267da7a9133a333ab222561a2
gonzoherme <gonzalo@hermenegildo.org>
2024-05-17 13:42:31 -0700

    xetex

--- config.el
+++ config.el
@@ -426,6 +426,13 @@
   :config
   (ultra-scroll-mac-mode 1))
 
+(use-package vterm
+  :commands vterm
+  :config
+  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *")  ;; Set this to match your custom shell prompt
+  (setq vterm-shell "zsh")                       ;; Set this to customize the shell to launch
+  (setq vterm-max-scrollback 10000))
+
 (use-package which-key
   :init
     (which-key-mode 1)
